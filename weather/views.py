0aa78133ea34bf7271919d2c9234308a59a44e79
aayushraj07 <aayush.97raj@gmail.com>
2019-03-23 19:12:06 +0530

    API initialised

--- weather/views.py
+++ weather/views.py
@@ -5,10 +5,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=717723aa9dd7b48d5c0cf15a343888c5'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperia&appid=717723aa9dd7b48d5c0cf15a343888c5'
     city = 'Las Vegas'
 
-    r = requests.get(url.format(city))
-    print(r.text)
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    print(city_weather)
 
     return render(request, 'weather/weather.html')
