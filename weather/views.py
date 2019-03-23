5d3a886ee0bc92e4a26fb9cb87e4928131f074fd
aayushraj07 <aayush.97raj@gmail.com>
2019-03-24 01:45:12 +0530

    API data showing on page

--- weather/views.py
+++ weather/views.py
@@ -1,22 +1,32 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 # Create your views here.
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperia&appid=717723aa9dd7b48d5c0cf15a343888c5'
-    city = 'Las Vegas'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=717723aa9dd7b48d5c0cf15a343888c5'
+    city = 'London'
 
-    r = requests.get(url.format(city)).json()
+    cities = City.objects.all()
 
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
+    weather_data = []
 
-    print(city_weather)
+    for city in cities:
 
-    return render(request, 'weather/weather.html')
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    print(weather_data)
+
+    context = {'city_weather': city_weather}
+    return render(request, 'weather/weather.html', context)
